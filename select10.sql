1008b8a4d124b070f18989eb2325008d42de07d2
123pik1 <s197736@student.pg.edu.pl>
2025-01-22 17:51:33 +0100

    Add new rental entry and update employee query to filter by last month's rentals

--- select10.sql
+++ select10.sql
@@ -7,5 +7,6 @@ FROM
 (Select Pracownicy.ID_pracownika AS ID, Osoby.Imie AS Imie, Osoby.Nazwisko AS Nazwisko, Pracownicy.Data_rozpoczecia_pracy AS Data_rozpoczecia_pracy
 From Pracownicy Join Osoby ON Pracownicy.ID_pracownika = Osoby.ID_osoby) AS Pr
 JOIN Wypozyczenia ON Pr.ID = Wypozyczenia.ID_pracownika
-GROUP BY Pr.ID, Pr.Imie, Pr.Nazwisko, Pr.Data_rozpoczecia_pracy
+GROUP BY Pr.ID, Pr.Imie, Pr.Nazwisko, Pr.Data_rozpoczecia_pracy, Wypozyczenia.Data_wypozyczenia
+HAVING DATEDIFF(MONTH, Wypozyczenia.Data_wypozyczenia, GETDATE()) = 1
 ORDER BY COUNT(Wypozyczenia.ID_rezerwacji) DESC;
